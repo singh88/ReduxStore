10a91d0a6710f40d613b402a2816864a5d8f7231
Manish Singh <manish.singh88@gmail.com>
2021-10-27 20:37:50 -0700

    some minor test updat

--- Package.swift
+++ Package.swift
@@ -21,6 +21,6 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "ReduxStore",
-            dependencies: [])
+            dependencies: ["RxSwift", "RxCocoa"])
     ]
 )
