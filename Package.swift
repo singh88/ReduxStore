78aecdabf572672009a1cc9c54a0a5ef7eff3673
Manish Singh <manish.singh88@gmail.com>
2021-12-18 13:17:33 -0800

    update some dependencies

--- Package.swift
+++ Package.swift
@@ -1,6 +1,5 @@
 // swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
@@ -9,16 +8,17 @@ let package = Package(
             .iOS(.v13)
         ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "ReduxStore",
             targets: ["ReduxStore"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "6.2.0")
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "6.0.0"),
+        .package(url: "https://github.com/RxSwiftCommunity/RxDataSources.git", from: "5.0.0")
     ],
     targets: [
-        .target(name: "ReduxStore", dependencies: ["RxSwift"])
+        .target(
+            name: "ReduxStore",
+            dependencies: ["RxSwift", .product(name: "RxCocoa", package: "RxSwift")])
     ]
 )
