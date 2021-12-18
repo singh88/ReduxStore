98739fe7f2010233a63db2e2259ff3c75d8fab98
Manish Singh <manish.singh88@gmail.com>
2021-12-18 10:32:17 -0800

    remove some dependence

--- Package.swift
+++ Package.swift
@@ -16,18 +16,9 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "6.0.0"),
-        .package(url: "https://github.com/RxSwiftCommunity/RxDataSources.git", from: "5.0.0")
-//        .package(url: "https://github.com/CombineCommunity/RxCombine.git", from: "2.0.0")
-
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "6.2.0")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "ReduxStore",
-            dependencies: ["RxSwift",
-                           .product(name: "RxCocoa", package: "RxSwift"),
-    "RxDataSources"])
+        .target(name: "ReduxStore", dependencies: ["RxSwift"])
     ]
 )
