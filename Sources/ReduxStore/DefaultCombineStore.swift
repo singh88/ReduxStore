2d39746526e6caf58d672b9f65d7764dbff8cd58
Manish Singh <manish.singh88@gmail.com>
2023-12-29 19:33:24 -0800

    Update DefaultCombineStore.swift
    add optional self check to avoid retain cycle

--- Sources/ReduxStore/DefaultCombineStore.swift
+++ Sources/ReduxStore/DefaultCombineStore.swift
@@ -95,7 +95,7 @@ public final class DefaultCombineStore<R: Reducer, RS: ReduxState,
 
     func printAllNextEvents() {
         _nextAction.forEach {
-            logEvents("currently \($0) is in queue")
+            self?.logEvents("currently \($0) is in queue")
         }
     }
 
